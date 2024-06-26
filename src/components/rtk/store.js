1ae8475afcca1b1c776038179f4ab777bb7900b4
helal0321 <159494281+helal0321@users.noreply.github.com>
2024-06-26 16:11:55 +0300

    Update store.js

--- src/components/rtk/store.js
+++ src/components/rtk/store.js
@@ -1,7 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { productReducer } from "./product_reducer";
 import { cartReducer } from "./cart_reducer";
-import { filtering } from "./priceReducer";
+import { filtering } from "./filter_reducer";
 
  export const Store=configureStore({
     reducer:{
